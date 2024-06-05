d2ecb845206654034807161494310ec9863dbca5
juliano valiati <160738358+juliano22052007@users.noreply.github.com>
2024-06-05 08:13:26 -0300

    arquivo

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaprincipal = document.queryselector(".caixa-princcipal");
+const caixaperguntas = document.queryselector(".caixa-perguntas");
+const caixaalternativas = document.queryselector(".caixa-alternativas");
+const caixaresultado= document.queryselector(".caixa-resultado");
+const textoresultado = document.queryselector(".texto-resultado");
